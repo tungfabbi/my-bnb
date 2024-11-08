ae412fecb14d5237bdfb84e96287c5b39fd37633
Tung <tungnd@fabbi-japan.jp>
2024-11-08 15:58:34 +0900

    add sign in sign out

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+'use client';
+
+import { SignOutButton } from '@clerk/nextjs';
+import { useToast } from "@/hooks/use-toast"
 
 function SignOutLink() {
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: 'You have been signed out.' });
+  };
   return (
-    <div>SignOutLink</div>
-  )
+    <SignOutButton redirectUrl='/'>
+      <button className='w-full text-left' onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
-
-export default SignOutLink
+export default SignOutLink;
