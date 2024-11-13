12cd89b8b197be3e18cf6f6766d64e7579fdd44b
Tung <tungnd@fabbi-japan.jp>
2024-11-13 17:11:57 +0900

    setup zod and prisma db

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,13 +1,7 @@
 import FormInput from '@/components/form/FormInput';
 import { SubmitButton } from '@/components/form/Buttons';
 import FormContainer from '@/components/form/FormContainer';
-
-const createProfileAction = async (prevState: any, formData: FormData) => {
-  'use server';
-  const firstName = formData.get('firstName') as string;
-  if (firstName !== 'test') return { message: 'there was an error...' };
-  return { message: 'Profile Created' };
-};
+import { createProfileAction} from '@/utils/actions';
 
 function CreateProfile() {
   return (
